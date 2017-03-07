a656a376c676a38e68e666520096927a724d2759
Jérémy WALTHER <jeremy.walther@golflima.net>
2017-03-07 22:41:21 +0100

    Pass headers to/from cURL, prevent access to big/slow files

--- src/Controllers/ApiController.php
+++ src/Controllers/ApiController.php
@@ -40,33 +40,50 @@ class ApiController
 
     public function proxy(Application $app, Request $request, $url, $contentType = "")
     {
+        // Decode parameters (b64 to prevent web server to think these calls are for files ...)
         $url = base64_decode(str_pad(strtr($url, '-_', '+/'), strlen($url) % 4, '=', STR_PAD_RIGHT));
         $contentType = base64_decode(str_pad(strtr($contentType, '-_', '+/'), strlen($contentType) % 4, '=', STR_PAD_RIGHT));
+        // Init cURL session
         $ch = curl_init($url);
         curl_setopt($ch, CURLOPT_HEADER, true);
-        // curl_setopt($ch, CURLOPT_HTTPHEADER, $request->headers);
+        $requestHeaders = array_map(function($value) {
+            return is_array($value) ? $value[0] : $value;
+        }, $request->headers->all());
+        curl_setopt($ch, CURLOPT_HTTPHEADER, $requestHeaders);
         curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
+        curl_setopt($ch, CURLOPT_MAXREDIRS, 10);
+        curl_setopt($ch, CURLOPT_TIMEOUT, 10); // 10 seconds allowed to do the download
+        curl_setopt($ch, CURLOPT_NOPROGRESS, false);
+        curl_setopt($ch, CURLOPT_PROGRESSFUNCTION, function(
+            $handle, $downloadSize, $downloaded, $uploadSize, $uploaded) {
+                return max($downloadSize, $downloaded) > (10 * 1024**2);
+        }); // Disallow the download of files bigger than 10 Mio
         $response = curl_exec($ch);
         if($response === FALSE) {
+            // Handle errors
             $response = new JsonResponse(array(
                 'cURL error code' => curl_errno($ch),
                 'cURL error message' => curl_error($ch),
                 'url' => $url,
                 'contentType' => $contentType,
-                'headers' => $request->headers
+                'headers' => $requestHeaders
             ), 404);
             curl_close($ch);
             return $response;
         }
         curl_close($ch);
+        // Split headers from content
         list($responseHeadersTmp, $responseContent) = preg_split('/(\r\n){2}/', $response, 2);
-        $responseHeadersTmp = preg_split('/(\r\n){1}/', $responseHeadersTmp);
+        // Parse headers
+        $responseHeadersTmp = preg_split('/\r\n/', $responseHeadersTmp);
         $responseHeaders = array();
-        foreach ($responseHeadersTmp as $name => $value) {
-            $responseHeaders[$name] = $value;
+        foreach ($responseHeadersTmp as $value) {
+            $header = preg_split('/:[[:blank:]]/', $value);
+            count($header) == 2 && $responseHeaders[$header[0]] = $header[1];
         }
         if ($contentType != "") {
-            $responseHeaders['Content-type'] = $contentType;
+            $responseHeaders['Content-Type'] = $contentType;
         }
         return new Response($responseContent, 200, $responseHeaders);
     }
